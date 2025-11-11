91eb46903a27d8dcdafc836f7891a1a6bb8033cf
Muhammad Hamada <muh.aref@live.com>
2025-11-11 21:43:38 +0100

    Update

--- app/GermanTelcB1App/src/config/active-exam.config.ts
+++ app/GermanTelcB1App/src/config/active-exam.config.ts
@@ -2,7 +2,7 @@
  * Active Exam Configuration
  * 
  * THIS FILE IS AUTO-GENERATED - DO NOT EDIT MANUALLY
- * Generated at: 2025-11-11T08:51:10.873Z
+ * Generated at: 2025-11-11T16:27:33.643Z
  * Exam: German TELC B1
  * 
  * This file determines which exam configuration is active for the current build.
