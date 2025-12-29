6037c413690b8df5e0b0e275357255096ef50ce4
Muhammad Hamada <muh.aref@live.com>
2025-12-29 20:05:56 +0100

    Speaking assessment

--- app/GermanTelcB1App/src/config/active-exam.config.ts
+++ app/GermanTelcB1App/src/config/active-exam.config.ts
@@ -2,7 +2,7 @@
  * Active Exam Configuration
  * 
  * THIS FILE IS AUTO-GENERATED - DO NOT EDIT MANUALLY
- * Generated at: 2025-12-29T16:30:04.069Z
+ * Generated at: 2025-12-29T19:02:32.390Z
  * Exam: German TELC B1
  * 
  * This file determines which exam configuration is active for the current build.
